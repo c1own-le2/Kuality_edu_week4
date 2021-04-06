0e8dd3913ea2f7be45df14f44df6d6b9fb78cff1
c1onw-le2 <sy41340@gmail.com>
2021-04-06 11:18:22 +0900

    re_bof1

--- 4week_pwnable/bof1.c
+++ 4week_pwnable/bof1.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
 void win(){
-	
 	system("/bin/sh");
-
 }
 
 int main(){
-	
 	char buf[8];
-	puts("Input : ");
-	gets(&buf);
-	puts("Good!");
 
+	gets(buf);
+	printf("Again!");
+
+	return 0;
 }
 
 
